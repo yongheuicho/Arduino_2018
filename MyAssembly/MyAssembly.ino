acc9493c25715a5a04f448034a429a23acc1b6af
yongheuicho <yongheuicho@gmail.com>
2018-03-21 10:24:00 +0900

    [2018-03-21] MyAssembly #1

--- MyAssembly/MyAssembly.ino
+++ MyAssembly/MyAssembly.ino
@@ -1,14 +1,18 @@
 #include <StringTok.h>
+#define SERIAL_BPS  (9600)
+#define DELAY_MS  (500)
 
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(SERIAL_BPS);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   String sInput = inputString();
-  processString(sInput);
+  double ans = processString(sInput);
+  printString(ans);
+  delay(DELAY_MS);
 }
 
 String inputString() {
@@ -16,9 +20,21 @@ String inputString() {
   return sInput;
 }
 
-void processString(String sInput) {
-  
+double processString(String sInput) {
+  double ans = 0.;
+  StringTok stToken;
+  return ans;
 }
 
+void printString(double ans) {
+  Serial.print("답 = "); Serial.println(ans);
+}
 
+double funAdd(double a, double b) {
+  return a + b;
+}
+
+double funSub(double a, double b) {
+  return a - b;
+}
 
